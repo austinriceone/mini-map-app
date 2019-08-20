1a5721fd0deadb638b3bf0999dd458f4e4e46d2c
ricema <austin.rice@dillards.com>
2019-08-20 10:07:29 -0500

    adjusting pipe

--- src/app/pipes/country-filter.pipe.ts
+++ src/app/pipes/country-filter.pipe.ts
@@ -1,13 +1,19 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import {Country} from "../dtos/country";
+import {Pipe, PipeTransform} from '@angular/core';
+import {Country} from '../dtos/country';
 
 @Pipe({
   name: 'countryFilter'
 })
 export class CountryFilterPipe implements PipeTransform {
 
-  transform(countriesToFilter: Array<Country>, args?: any): any {
-    return countriesToFilter.filter(country => Object.keys(country).some(key => country[key] ? country[key].toString().toLowerCase().includes(args.toString().toLowerCase()) : false));
+  transform(countriesToFilter: Array<Country>, term?: any): any {
+    return countriesToFilter.filter(country => {
+      const countryKeys = Object.keys(country);
+      countryKeys.some(key => {
+        const value = country[key].toString().trim().toLowerCase();
+        return value ? value.includes(term.toString().trim().toLowerCase()) : false;
+      });
+    });
   }
 
 }
