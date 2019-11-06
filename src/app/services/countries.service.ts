fee8ea9582879ab8f2d0d52452074eecaf5885e3
ricema <austin.rice@dillards.com>
2019-11-06 06:52:57 -0600

    [ricema] adding fallback when service is down

--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { catchError, map } from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {catchError, map} from 'rxjs/operators';
+import * as countryJsonObject from "./backup-data/countryJsonObject.json";
+import {of} from "rxjs";
 import {Country} from "../dtos/country";
 
 
@@ -8,12 +10,19 @@ import {Country} from "../dtos/country";
 export class CountriesService {
 
   corsProxy = 'https://cors-anywhere.herokuapp.com/';
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {
+  }
 
   getCountries(): any {
     return this.http.get(this.corsProxy + `https://countryapi.gear.host/v1/Country/getCountries`).pipe(
-      map((resp: any) => {return resp.Response.map(countryData => new Country(countryData))}),
-      catchError(error => error)
+      catchError(response => {
+        return of(countryJsonObject.Response);
+      }),
+      map((response: any) => {
+          return response.Response.map(country => new Country(country))
+        }
+      )
     )
   }
 }
